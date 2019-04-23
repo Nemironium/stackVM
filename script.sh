8d0d0139173f05aa05b6fa588ee65ff251e1664f
Nemironium <nemiron@outlook.com>
2019-04-24 00:00:30 +0300

    Updated for interpretator; Added time calculation

--- script.sh
+++ script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
+SECONDS=0
+
 ./preprocessor $1
 result=$(./analyzer "$1.temp" > "$1.zasm" 2> "err.log")
 if [[  $(grep 'Error' err.log) ]]; then 
     cat err.log
-    rm -f "$1.zasm"
 else
     cat err.log
+    echo $'\n'$1$' output:\n'
+    ./interpretator "$1.zasm"
 fi
+
+rm -f "$1.zasm"
 rm -f err.log
 rm -f "$1.temp" 
+
+duration=$SECONDS
+echo $'\n'
+echo $"[Finished in $(($duration % 60))s]"
